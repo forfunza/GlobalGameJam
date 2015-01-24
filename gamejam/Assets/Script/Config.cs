896b2bfa6cef5a9c849bd8a38197d92cd4fa6e03
Wutthichai <limplemoness@gmail.com>
2015-01-25 03:32:32 +0700

    gameplay

--- gamejam/Assets/Script/Config.cs
+++ gamejam/Assets/Script/Config.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Config {
 
 	public static int SIZE_OF_GRID = 4;
 	public static int[] TYPE_LIST = new int[]{1,2,3};
+	public static int SIZE_OF_BOX = 4;
+	public static float TIME_GAMEPLAY = 10.0F;
+
 }
---
+++ gamejam/Assets/Script/Gameplay/ScoreText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour
+{
+
+		private string text;
+
+		public string SetText {
+				get {
+						return text;
+				}
+				set {
+						text = value;
+				}
+		}
+
+
+}

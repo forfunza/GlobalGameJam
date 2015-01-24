cad9b2eccc532382c206496f9795b219ecb69497
Wutthichai <limplemoness@gmail.com>
2015-01-24 15:29:27 +0700

    move file into folder

--- gamejam/Assets/Script/Gameplay/Gameplay.cs
+++ gamejam/Assets/Script/Gameplay/Gameplay.cs
@@ -2,15 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Grid : MonoBehaviour
+public class Gameplay: MonoBehaviour
 {
-
-	
 		public GameObject tilePrefab;
-		public int numberOfTiles = 16;
-		public int tilesPerRow = 4;
 		public float distanceBetweenTiles = 1.0f;
-//	public int numberOfMines = 10;
 		private Sprite myTile;
 		public enum State
 		{
@@ -18,19 +13,13 @@ public class Grid : MonoBehaviour
 				Ready = 1
 		}
 
-
-
 		public int state;
 		private int xIndex;
 		private int yIndex;
 		private int xTempIndex;
 		private int yTempIndex;
-		private GameObject[,] _tiles;
 		private Dictionary<string,GameObject> _tilesBox = new Dictionary<string,GameObject> ();
-//	
-//	static Tile[] tilesAll;
-//	static Array tilesMined;
-//	static Array tilesUnmined;
+
 		// Use this for initialization
 		void Start ()
 		{
@@ -47,19 +36,18 @@ public class Grid : MonoBehaviour
 
 		void CreateTiles ()
 		{
-//		tilesAll = new Tile[numberOfTiles];
-//		tilesMined = new Array();
-//		tilesUnmined = new Array();
 		
 				float xOffset = 0.0f;
 				float yOffset = 0.0f;
 
-				_tiles = new GameObject[4, 4];
-				for (int rows = 0; rows < 4; rows++) {
-		
-						for (int cols = 0; cols < 4; cols++) {
 
-			
+				TileModel[,] _allTiles = GameEngine.RandomTilesList ();
+
+				for (int rows = 0; rows < Config.SIZE_OF_GRID; rows++) {
+		
+						for (int cols = 0; cols < Config.SIZE_OF_GRID; cols++) {
+
+								Debug.Log (_allTiles [cols, rows]);
 
 								xOffset += distanceBetweenTiles;
 
@@ -67,41 +55,26 @@ public class Grid : MonoBehaviour
 								Tile _tile = _tileObject.GetComponent<Tile> ();
 								SpriteRenderer _sprite = _tileObject.GetComponent<SpriteRenderer> ();
 								_sprite.sprite = myTile;
-								_tile.x = cols;
-								_tile.y = rows;
-								_tile.type = 1;
-								if (rows == 2 && cols == 1) {
-										_tile.type = 0;
-										_sprite.sprite = null;
-								}
+								_tile.x = _allTiles [cols, rows].x;
+								_tile.y = _allTiles [cols, rows].y;
+								_tile.type = _allTiles [cols, rows].type;
 
 								if (_tile.type == 0) {
-										xIndex = cols;
-										yIndex = rows;
+										xIndex = _allTiles [cols, rows].x;
+										yIndex = _allTiles [cols, rows].y;
+					_sprite.sprite = null;
 								}
-							
 
 								_tilesBox.Add (cols.ToString () + rows.ToString (), _tileObject);
-		
-
 						}
 						yOffset -= distanceBetweenTiles;
 						xOffset = 0;
 				}
 
-
-
-
 		}
 
 		public void checkMoveAble (int direction)
 		{
-
-				// 0: up, 1: right, 2:down, 3: left
-				//Debug.Log (direction);
-
-//		Debug.Log("Old : "+ xIndex + " " + yIndex);
-		
 				if (direction == 0) {
 						xTempIndex = xIndex;
 						yTempIndex = yIndex + 1;
@@ -120,14 +93,12 @@ public class Grid : MonoBehaviour
 						Debug.Log ("LEFT");
 				}
 
-//		Debug.Log("New : "+ xTempIndex + " " + yTempIndex);
+
 
 				if (_tilesBox.ContainsKey (xTempIndex.ToString () + yTempIndex.ToString ())) {
 
 						moveTile (xTempIndex, yTempIndex, xIndex, yIndex);
-//
-//						Debug.Log ("From X : " + xTempIndex + " Y : " + yTempIndex);
-//						Debug.Log ("To X : " + xIndex + " Y : " + yIndex);
+
 				}
 		
 		
@@ -144,25 +115,26 @@ public class Grid : MonoBehaviour
 				GameObject _blankTileObject = _tilesBox [x2.ToString () + y2.ToString ()];
 //				_moveTileObject.transform.position = _blankTileObject.transform.position;
 				
-				Hashtable ht = new Hashtable();
-				ht.Add("x",_blankTileObject.transform.position.x);
-				ht.Add("y",_blankTileObject.transform.position.y);
-				ht.Add("time",0.3);
-				ht.Add("onComplete","tweenComplete");
-				ht.Add("onCompleteTarget",gameObject);
-				iTween.MoveTo(_moveTileObject,ht);
+				Hashtable ht = new Hashtable ();
+				ht.Add ("x", _blankTileObject.transform.position.x);
+				ht.Add ("y", _blankTileObject.transform.position.y);
+				ht.Add ("time", 0.3);
+				ht.Add ("onComplete", "tweenComplete");
+				ht.Add ("onCompleteTarget", gameObject);
+				iTween.MoveTo (_moveTileObject, ht);
 
 				_blankTileObject.transform.position = _previousMovePosition;
 
 				xIndex = x1;
 				yIndex = y1;
-				_tilesBox[x1.ToString () + y1.ToString ()] = _blankTileObject;
-				_tilesBox[x2.ToString () + y2.ToString ()] = _moveTileObject;
+				_tilesBox [x1.ToString () + y1.ToString ()] = _blankTileObject;
+				_tilesBox [x2.ToString () + y2.ToString ()] = _moveTileObject;
 		
 
 		}
 
-		void tweenComplete(){
+		void tweenComplete ()
+		{
 //				Debug.Log("aaaa");
 				state = (int)State.Ready;
 		}

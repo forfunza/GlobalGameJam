d509acabc6c5562724e85d9cbf139a605f18778d
Amornthep <illusion.zen@gmail.com>
2015-01-25 03:51:27 +0700

    config

--- gamejam/Assets/Script/Config.cs
+++ gamejam/Assets/Script/Config.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Config {
 
 	public static int SIZE_OF_GRID = 4;
@@ -8,4 +8,9 @@ public class Config {
 
 
 	public static string STAR_OF_STATE = "STAR_OF_STATE";
+	public static Dictionary<int, TileModel[]> boxTileStage = new Dictionary<int, TileModel[]>()
+	{
+		{1,new TileModel[]{new TileModel(4,0,0),new TileModel(4,1,1),new TileModel(4,2,2),new TileModel(4,3,3)}},
+		{2,new TileModel[]{new TileModel(4,0,0),new TileModel(4,1,1),new TileModel(4,2,2),new TileModel(4,3,3)}}
+	};
 }

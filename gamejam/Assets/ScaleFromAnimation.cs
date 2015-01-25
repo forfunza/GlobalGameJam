b896d32e8464d73c797d88d86ffd8d8c0cf03a7a
AtitJeungniponsakul <atit.jeu@github.com>
2015-01-25 11:41:42 +0800

    Update Main Scene

---
+++ gamejam/Assets/ScaleFromAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleFromAnimation : MonoBehaviour {
+	public Vector3 gg;
+	public float time;
+	public iTween.EaseType easeType;
+	public iTween.LoopType loopType;
+
+	// Use this for initialization
+	void Start () {
+		gg = new Vector3(0.25f,0.30f);
+		ScaleSmooth();
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ScaleSmooth() {
+		iTween.ScaleAdd(this.gameObject,iTween.Hash("amount" ,gg,"time",time,"easeType",easeType.ToString(),"loopType",loopType.ToString()));  
+
+	}
+
+}
--- gamejam/Assets/Script/ManagerButton.cs
+++ gamejam/Assets/Script/ManagerButton.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class ManagerButton : MonoBehaviour {
-
+	public GameObject title;
+	public GameObject button;
+	public Vector3 amount;
+	public float time;
+	public float waitTime;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,7 +20,19 @@ public class ManagerButton : MonoBehaviour {
 	{
 		Application.LoadLevel(sceneName);
 	}
+	public void TitleOut () {
+		StartCoroutine(WaitASec(waitTime));
+	}
+	IEnumerator WaitASec(float waitTime){
+		yield return new WaitForSeconds(waitTime);
+		iTween.MoveAdd(title,iTween.Hash("amount" ,amount,"time",time)); 
+		iTween.MoveAdd(button,iTween.Hash("amount" ,amount,"time",time));
+
+		Debug.Log ("Waited a sec");
+	}
 	public void QuitGame (){
 		Application.Quit();
 	}
+
+
 }

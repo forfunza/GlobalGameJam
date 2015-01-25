c1addc5c356fc578070b17b16698e2d7bbebab2f
piyaphat <piyaphat.u@gmail.com>
2015-01-25 10:49:42 +0700

    Show popup

--- gamejam/Assets/Script/ManagerButton.cs
+++ gamejam/Assets/Script/ManagerButton.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ManagerButton : MonoBehaviour {
 
+	public GameObject popupMap;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,5 +20,20 @@ public class ManagerButton : MonoBehaviour {
 	}
 	public void QuitGame (){
 		Application.Quit();
+	}
+
+	public void selectWorld(){
+		GameObject go = (GameObject)Instantiate (popupMap, transform.parent.transform.position, transform.parent.transform.rotation);
+		go.transform.parent = transform.parent.transform;
+		go.transform.localScale = new Vector3(2.061856f,2.061856f,2.061856f);
+
+		Hashtable ht = new Hashtable ();
+		ht.Add ("scale",10.061856f);
+		ht.Add ("time", 3);
+		ht.Add ("easetype","linear");
+//		ht.Add ("onComplete", "tweenComplete");
+//		ht.Add ("onCompleteTarget", gameObject);
+		iTween.ScaleFrom (this.gameObject, ht);
+
 	}
 }

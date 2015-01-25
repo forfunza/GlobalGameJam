f22ca157b8f88b76e46eb1115e44e53087ee8e83
Wutthichai <limplemoness@gmail.com>
2015-01-25 08:16:44 +0700

    config

--- gamejam/Assets/Script/Config.cs
+++ gamejam/Assets/Script/Config.cs
@@ -2,27 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Config {
+public class Config
+{
 
-	public static int SIZE_OF_GRID = 4;
-	public static int[] TYPE_LIST = new int[]{1,2,3};
+		public static int SIZE_OF_GRID = 4;
+		public static int[] TYPE_LIST = new int[]{1,2,3};
+		public static int SIZE_OF_BOX = 4;
+		public static string STAR_OF_STATE = "STAR_OF_STATE";
+		public const string ALARM = "Alarm";
+		public const string BGM_MAIN_MENU = "BgmMainMenu";
+		public const string TILE_MISSION = "TileMission";
+		public const string TILE_MOVE = "TileMove";
+		public const string PRESS_BUTTON = "PressButton";
+		public const string OPEN_POPUP = "OpenPopup";
+		public const string ERROR_MOVE = "ErrorMove";
+		public const string WIN_GAME = "WinGame";
+		public const string CLOSE_POPUP = "ClosePopup";
+		public const string GAMEOVER = "Gameover";
+		public const string GAMEPLAY_BGM = "GameplayBgm";
+		
+		public const string SOUND = "Sound/";
 
-public static int SIZE_OF_BOX = 4;
-	public static string STAR_OF_STATE = "STAR_OF_STATE";
 
-	public const string ALARM = "Alarm";
-	public const string BGM_MAIN_MENU = "BgmMainMenu";
-	public const string TILE_MISSION = "TileMission";
-	public const string TILE_MOVE = "TileMove";
-	public const string PRESS_BUTTON = "PressButton";
-	public const string OPEN_POPUP = "OpenPopup";
-	public const string ERROR_MOVE = "ErrorMove";
-	public const string WIN_GAME = "WinGame";
-	public const string CLOSE_POPUP = "ClosePopup";
-	public const string GAMEOVER = "Gameover";
-	public const string GAMEPLAY_BGM = "GameplayBgm";
-public static float TIME_GAMEPLAY = 10.0F;
-	public const string SOUND = "Sound/";
 
+	public static float TIME_GAMEPLAY = 90.0F;
+	
 
 }
